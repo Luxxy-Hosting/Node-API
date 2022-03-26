3ce0d6bdf4c376253fe987b115312f4fc8ba7136
Luxxy-GF <deniedaddicted@gmail.com>
2022-03-26 22:28:01 +0000

    config ports

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const dir = __dirname
 const app = express()
 const apirouter = require("./routes/apirouter")
 
+const httpport = 998
+const httpsport = 997
 
 app.get('/', function(req, res){ res.json({ status: 200, message: 'what are you doing here :)'  }) });
 app.use('/api', apirouter)
@@ -15,5 +17,5 @@ app.use('/api', apirouter)
 const privateKey = fs.readFileSync(`${dir}/certs/privkey.pem`);
 const certificate = fs.readFileSync(`${dir}/certs/fullchain.pem`);
 
-app.listen(998, () => { console.log(`http server running on 998`) })
-https.createServer( { key: privateKey, cert: certificate }, app).listen(997, ()=> { console.log('https server running on 997') });
+app.listen(httpport, () => { console.log(`http server running on 998`) })
+https.createServer( { key: privateKey, cert: certificate }, app).listen(httpsport, ()=> { console.log('https server running on 997') });
